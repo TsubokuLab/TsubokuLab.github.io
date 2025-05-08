1f316e97b1c32ff5db6152c9573ac54e92c2a026
Teruaki Tsubokura <spinning.spirit@gmail.com>
2025-05-08 18:18:00 +0900

    Updates

---
+++ setup-server.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Installing local server package...
+npm install --save-dev http-server
+echo Installation completed!
+echo.
+echo Next, run 'start-server.bat' to begin local testing.
+pause
---
+++ start-server.bat
@@ -0,0 +1,8 @@
+@echo off
+echo Starting TsubokuLab GitHub Pages for local testing...
+echo.
+echo Access the site at: http://localhost:8080
+echo.
+echo To exit, press Ctrl+C in this window.
+echo.
+npx http-server -o -p 8080
